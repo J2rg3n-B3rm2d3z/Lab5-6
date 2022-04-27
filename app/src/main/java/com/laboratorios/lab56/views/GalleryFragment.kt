6aeb99ad4cfadea9c104f6eae8a6fc1e4e9d17a8
Mitch-coder <michellescalderon2002@gmail.com>
2022-04-26 22:49:18 -0600

    Changing interfaces b3

--- app/src/main/java/com/laboratorios/lab56/views/GalleryFragment.kt
+++ app/src/main/java/com/laboratorios/lab56/views/GalleryFragment.kt
@@ -50,12 +50,12 @@ class GalleryFragment : Fragment(), galeriaListener {
     //Get the List to use in the recycler view
 
     private fun getGaleria(): MutableList<pintura>{
+        // we have to create a list with drivers of f1
         val galeriaList: MutableList<pintura> = ArrayList()
-        galeriaList.add(pintura("Haniel Herrera","1,600","https://artelista.s3.amazonaws.com/obras/big/3/5/5/1209702-607458d7c0c2a.jpg","Memories"))
-        galeriaList.add(pintura("Jesús Cuenca","1,200", "https://artelista.s3.amazonaws.com/obras/big/1/6/4/1209582.jpg", "Rancio"))
-        galeriaList.add(pintura("Pol Ledent", "1,600", "https://artelista.s3.amazonaws.com/obras/fichas/1/4/6/1209343.jpg", "Como en invierno"))
-        galeriaList.add(pintura("Maribel Flores", "1,600", "https://artelista.s3.amazonaws.com/obras/big/7/4/1/1228034.jpg", "Blossom"))
-        galeriaList.add(pintura("Nana Tchelidze", "575", "https://artelista.s3.amazonaws.com/obras/big/1/1/4/1228154.jpg", "El Puente"))
+        galeriaList.add(pintura("Autódromu","1997-2003","https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Red_Bull_Ring%2C_April_18%2C_2018_SkySat.jpg/1280px-Red_Bull_Ring%2C_April_18%2C_2018_SkySat.jpg","Red Bull Ring"))
+        galeriaList.add(pintura("Hibirido","2004-2010", "https://upload.wikimedia.org/wikipedia/commons/1/10/2012_Bahrain_Grand_Prix_2.jpg", "Bahrain Grand Prix"))
+        galeriaList.add(pintura("Autódromu", "1999", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/F1_Circuit_de_Catalunya_-_Tribuna.jpg/1024px-F1_Circuit_de_Catalunya_-_Tribuna.jpg", "Circuito de Catalunya"))
+        galeriaList.add(pintura("Autódromu", "2009", "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Autodromo_aerea_poster.jpg/1024px-Autodromo_aerea_poster.jpg", "Autodromo Aerea"))
         return galeriaList
     }
 
