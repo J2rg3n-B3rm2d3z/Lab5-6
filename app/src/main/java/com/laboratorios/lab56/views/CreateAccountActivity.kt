47ca8884169f773c3ea860cb21bd1492d2e72eb4
Jurgen hace programas <jurgenfbermudez@gmail.com>
2022-04-27 01:33:08 -0600

    First Part Finished

--- app/src/main/java/com/laboratorios/lab56/views/CreateAccountActivity.kt
+++ app/src/main/java/com/laboratorios/lab56/views/CreateAccountActivity.kt
@@ -2,6 +2,7 @@ package com.laboratorios.lab56.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import com.laboratorios.lab56.databinding.ActivityCreateAccountBinding
@@ -28,10 +29,95 @@ class CreateAccountActivity : AppCompatActivity() {
 
         //Event to get a button
 
+        emailFocusListener()
+        passswordFocusListener()
+        passwordConfirmFocusListener()
+
         binding.btnCrearCuenta.setOnClickListener {
-            Toast.makeText(this, "El usuario ha sido creado", Toast.LENGTH_SHORT).show()
-            finish()
+
+            val userText = binding.tvEmailCuenta.text.toString()
+            val passwordText = binding.tvPasswordCuenta.text.toString()
+            val passwordTextConfirmar = binding.tvConfirmarPassword.text.toString()
+
+            if(Patterns.EMAIL_ADDRESS.matcher(userText).matches() && passwordText.length >= 3 &&
+                passwordText.length <= 8 &&
+                (passwordText.matches(".*[a-z].*".toRegex()) ||
+                        passwordText.matches(".*[A-Z].*".toRegex())) &&
+                passwordText.matches(".*[0-9].*".toRegex()) &&
+                passwordText == passwordTextConfirmar) {
+
+                Toast.makeText(this, "El usuario ha sido creado", Toast.LENGTH_SHORT).show()
+                finish()
+            }
         }
 
     }
+
+    private fun emailFocusListener() {
+        binding.tvEmailCuenta.setOnFocusChangeListener { _, focused ->
+            if(!focused)
+                binding.tvEmailCuentaContainer.helperText = validEmail()
+        }
+    }
+
+    private fun validEmail(): String? {
+        val userText = binding.tvEmailCuenta.text.toString()
+
+        return if(!Patterns.EMAIL_ADDRESS.matcher(userText).matches())
+            "E-mail invalido"
+        else if(userText=="")
+            "Requerido"
+        else
+            null
+    }
+
+
+
+    private fun passswordFocusListener() {
+        binding.tvPasswordCuenta.setOnFocusChangeListener { _, focused ->
+            if(!focused)
+                binding.tvPasswordCuentaContainer.helperText = validpasssword()
+        }
+    }
+
+    private fun validpasssword(): String? {
+        val passwordText = binding.tvPasswordCuenta.text.toString()
+
+        return if(passwordText.length<3)
+            "Minimo 3 caracteres"
+        else if(passwordText.length>8)
+            "Maximo 8 caracteres"
+        else if(!passwordText.matches(".*[a-z].*".toRegex()) &&
+            !passwordText.matches(".*[A-Z].*".toRegex()))
+            "Debe tener letras"
+        else if(!passwordText.matches(".*[0-9].*".toRegex()))
+            "Debe tener digitos"
+        else if(passwordText=="")
+            "Requerido"
+        else
+            null
+    }
+
+
+    private fun passwordConfirmFocusListener() {
+        binding.tvConfirmarPassword.setOnFocusChangeListener { _, focused ->
+            if(!focused)
+                binding.tvConfirmarPasswordContainer.helperText = validpasswordConfirm()
+        }
+    }
+
+    private fun validpasswordConfirm(): String? {
+        val passwordTextConfirmar = binding.tvConfirmarPassword.text.toString()
+
+        return if(passwordTextConfirmar != binding.tvPasswordCuenta.text.toString())
+            "La Contraseña no es la misma"
+        else if(passwordTextConfirmar=="")
+            "Requerido"
+        else
+            null
+    }
+
 }
+
+
+
--- app/src/main/java/com/laboratorios/lab56/views/LoginActivity.kt
+++ app/src/main/java/com/laboratorios/lab56/views/LoginActivity.kt
@@ -3,8 +3,11 @@ package com.laboratorios.lab56.views
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.view.View
+import com.laboratorios.lab56.R
 import com.laboratorios.lab56.databinding.ActivityLoginBinding
+import java.util.regex.Pattern
 
 class LoginActivity : AppCompatActivity() {
 
@@ -18,14 +21,71 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        emailFocusListener()
+        passswordFocusListener()
 
+    }
+
+    private fun emailFocusListener() {
+        binding.tvUserName.setOnFocusChangeListener { _, focused ->
+            if(!focused)
+                binding.tvUserNameContainer.helperText = validEmail()
+        }
+    }
+
+    private fun validEmail(): String? {
+        val userText = binding.tvUserName.text.toString()
+
+        return if(!Patterns.EMAIL_ADDRESS.matcher(userText).matches())
+            "E-mail invalido"
+        else if(userText=="")
+            "Requerido"
+        else
+            null
+    }
+
+    private fun passswordFocusListener() {
+        binding.password.setOnFocusChangeListener { _, focused ->
+            if(!focused)
+                binding.passwordContainer.helperText = validpasssword()
+        }
+    }
+
+    private fun validpasssword(): String? {
+        val passwordText = binding.password.text.toString()
+
+        return if(passwordText.length<3)
+            "Minimo 3 caracteres"
+        else if(passwordText.length>8)
+            "Maximo 8 caracteres"
+        else if(!passwordText.matches(".*[a-z].*".toRegex()) &&
+            !passwordText.matches(".*[A-Z].*".toRegex()))
+            "Debe tener letras"
+        else if(!passwordText.matches(".*[0-9].*".toRegex()))
+            "Debe tener digitos"
+        else if(passwordText=="")
+            "Requerido"
+        else
+            null
     }
 
     //Function to button when click in Login
 
     fun onClickArteLista (view: View){
-        val intent = Intent(this, MenuActivity::class.java)
-        startActivity(intent)
+
+        val passwordText = binding.password.text.toString()
+        val userText = binding.tvUserName.text.toString()
+
+        if(Patterns.EMAIL_ADDRESS.matcher(userText).matches() && passwordText.length >= 3 &&
+            passwordText.length <= 8 &&
+            (passwordText.matches(".*[a-z].*".toRegex()) ||
+                    passwordText.matches(".*[A-Z].*".toRegex())) &&
+            passwordText.matches(".*[0-9].*".toRegex())){
+
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+
+        }
     }
 
     //Function to button to switch a createAccount Activity
